cc2b45c02888ecd54f0fdef665080755dde3cd0d
mowslasher83 <radeswaled@hotmail.com>
2025-08-31 10:14:48 -0500

    Added export for the Tooltip component.

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ export { Button } from './components/Button';
 export { Input } from './components/Input';
 export { Modal } from './components/Modal';
 export { Notification } from './components/Notification';
+export { Tooltip } from './components/Tooltip';
