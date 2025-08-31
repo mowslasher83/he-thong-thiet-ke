c9448b275697af3cdb2755a834e2b21eb2b4b55b
mowslasher83 <radeswaled@hotmail.com>
2025-08-31 10:14:57 -0500

    Consolidate exports into a single line for cleaner code.

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-export { Button } from './components/Button';
-export { Input } from './components/Input';
-export { Modal } from './components/Modal';
-export { Notification } from './components/Notification';
-export { Tooltip } from './components/Tooltip';
+export { Button, Input, Modal, Notification, Tooltip } from './components';
